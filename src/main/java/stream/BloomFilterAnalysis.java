592b5078c5be88002250a84f556bdc8eebe90e6f
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2021-06-02 09:04:16 +0200

    Added description and possible encoding and hashing interface

--- src/main/java/stream/BloomFilterAnalysis.java
+++ src/main/java/stream/BloomFilterAnalysis.java
@@ -2,19 +2,17 @@ package stream;
 
 import org.apache.spark.streaming.api.java.JavaDStream;
 import scala.Function1;
-import scala.Int;
+import scala.Function2;
 import scala.Tuple2;
 
-import java.io.Serializable;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
- *
+ * A bloom filter have been implemented in order to not allow the ingestion of particular data.
+ * Let's suppose that the stream source is producing some neighbourhood that we don't want consider during the analysis.
+ * The purpose of this Bloom Filter is exactly block that kind of data.
  */
 public class BloomFilterAnalysis implements DataStreamAnalysis {
 
@@ -33,5 +31,8 @@ public class BloomFilterAnalysis implements DataStreamAnalysis {
                 .mapToPair(e -> new Tuple2<>(e._1, bitMap.get(e._2 % M) ? "BLOCKED" : "PASS"))
                 .print();
     }
+
+    public interface Hashing extends Function2<Integer, Integer, Integer> {};
+    public interface Encoding extends Function1<String, Integer> {};
 
 }
